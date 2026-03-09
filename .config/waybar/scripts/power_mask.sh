23e0f9aad8922c109d31f0b1b15a2d5e9046bbeb
Alessandro Perazzetta <482310+AlessandroPerazzetta@users.noreply.github.com>
2026-03-09 16:58:18 +0100

    Add power mask functionality to Waybar with custom scripts and styles to enable/disable suspend (hypridle)

---
+++ .config/waybar/scripts/power_mask.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Check if hypridle is currently running
+if pgrep -x "hypridle" > /dev/null; then
+    IS_RUNNING=true
+else
+    IS_RUNNING=false
+fi
+
+if [[ "$1" == "toggle" ]]; then
+    if [ "$IS_RUNNING" = true ]; then
+        # Kill hypridle to prevent sleep (Inhibit ON)
+        pkill hypridle
+        notify-send "Hyprland" "Sleep Inhibited (Caffeine ON)" -i nightly
+    else
+        # Restart hypridle to allow sleep (Inhibit OFF)
+        hypridle &
+        notify-send "Hyprland" "Sleep Enabled (Caffeine OFF)" -i sleep
+    fi
+else
+    # Output for Waybar (Logic is flipped: Running = Normal icon, Not Running = Inhibited icon)
+    if [ "$IS_RUNNING" = true ]; then
+        echo '{"text": "󰒲", "class": "normal", "tooltip": "Hypridle Active: System will sleep"}'
+    else
+        echo '{"text": "󰛐", "class": "inhibited", "tooltip": "Hypridle Inactive: System will stay awake"}'
+    fi
+fi
